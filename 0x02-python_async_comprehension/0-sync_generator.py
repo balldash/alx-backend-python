256b24831bb53be1bfde2554dc9b96274fcf954b
Bislon Zulu <bislonzulu1@outlook.com>
2024-10-15 19:52:38 +0200

    tasks

---
+++ 0x02-python_async_comprehension/0-sync_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+An Async generator.
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator -> Generator[float, None, None]:
+    """
+    A function that has a coroutine looping 10 times.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+An Async comprehension module.
+"""
+import asyncio
+from typing import List
+asynct_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List(float):
+    """
+    A function will collect 10 random numbers and return them.
+    """
+    values = [i async for i in async_generator()]
+    return values
